fd7e4b1cb24cc874387806ba0a2d4a2915cdfd42
Ace <ace.lowder@gmail.com>
2024-07-09 16:29:31 -0700

    Fix depreciated ant design component props

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import 'antd/dist/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
