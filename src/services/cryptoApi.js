97b75ba123c19cd39deff3444afb83932ca86520
Ace <ace.lowder@gmail.com>
2024-07-11 20:12:57 -0700

    Remove exchanges api call

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,10 +21,6 @@ export const cryptoApi = createApi({
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timePeriod=${timeperiod}`),
     }),
-
-    getExchanges: builder.query({
-      query: () => createRequest('/exchanges'),
-    }),
   }),
 });
 
@@ -32,5 +28,4 @@ export const {
   useGetCryptosQuery,
   useGetCryptoDetailsQuery,
   useGetCryptoHistoryQuery,
-  useGetExchangesQuery,
 } = cryptoApi;
